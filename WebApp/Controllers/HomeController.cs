51c9d4a2e20ae561cbe024fc74f6b6fcd1fc7566
Swapnil Shah <swapnil.shah@clariontechnologies.co.in>
2017-07-05 12:00:15 +0530

    Changes the home controller

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hi Swapnil, Your application description page.";
 
             return View();
         }
